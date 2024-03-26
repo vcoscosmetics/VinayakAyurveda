913faa92fffe0722ebc6c61a714b68da02211507
Sunnyvats01 <sunnyvats1201@gmail.com>
2024-03-26 17:11:43 +0530

    navbar mobile version

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const burger = document.querySelector(".burger");
-const navlist = document.querySelector(".nav-list");
+const navlist = document.querySelector(".dropdown-menu");
 const navbar = document.querySelector(".navbar");
+
+
+console.log(navlist);
 
 burger.addEventListener("click", () => {
   navlist.classList.toggle("v-class");
-  navbar.classList.toggle("h-nav");
 });
 
 
