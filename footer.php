269eaf9a815fe870993993ffdbeb81f1a943a4bb
vcoscosmetics <vcoscosmetics@gmail.com>
2024-06-11 16:41:46 +0530

    save

---
+++ footer.php
@@ -0,0 +1,49 @@
+<footer>
+      <div class="foot-panel2">
+        <ul>
+          <p>Quick Links</p>
+          <a href="index.html">Home</a>
+          <a href="aboutUs.html">About us</a>
+          <a href="ourproducts.html">Our Products</a>
+          <a href="PCDpharma.html">  PCD Pharma Franchise </a>
+          <a href="thirdparty.html">Third Party Manufacturing</a>
+          <a href="ContactUs.html">Contact Us</a>
+          
+        </ul>
+
+        <ul>
+          <p>Services</p>
+          <a>Private Lables</a>
+          <a>Cosmetics thirdparty Manufacturing</a>
+          <a>Ayurvedic Medicine</a>
+          <ul class="footer-inner-list">
+            <li><a href=""><i class="fa-brands fa-instagram "></i></a></li>
+            <li><a href=""><i class="fa-brands fa-facebook "></i></a></li>
+            <li><a href=""><i class="fa-brands fa-x-twitter "></i></a></li>
+            <li><a href="https://wa.me/9870579944"><i class="fa-brands fa-whatsapp"></i></a></li>
+          </ul>
+        </ul>
+
+        <ul>
+          <p>Contact Info</p>
+          <a> Vinayak Ayurveda Pvt. Ltd.</a>
+          <a>Plot No. 12 New DLF Industrial Area</a>
+          <a> Phase 1, Faridabad(Haryana)</a>
+          <a>Pin-121003 </a>
+          <a>Mobile: +91 9870579944</a>
+          <a>Email-info@vinayakayurveda.in</a>
+          
+        </ul>
+      </div>
+      <div class="foot-panel4">
+        <div class="pages">
+          <a>Conditions of Use</a>
+          <a>Privacy</a>
+          <a>Your Ads Privacy Choices</a>
+        </div>
+        <div class="copyright">
+          All Right Reserved @ vinayakayurveda.Web Design & Development By
+          Vinayak Ayurveda
+        </div>
+      </div>
+    </footer>
---
+++ index.php
@@ -0,0 +1,359 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Vinayak Ayurveda</title>
+    <link rel="icon" type="image/x-icon" href="favicon-32x32.png">
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/media-queries.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+
+    
+  </head>
+  <body>
+    <div class="main">
+      <header>
+        <div class="Nav1">
+          <div class="nav1-left">
+            <marquee>Welcome to Vinayak Ayurveda Pvt. Ltd.</marquee>
+          </div>
+          <div class="nav1-right">
+            <i class="fa-solid fa-phone fa-rotate-270" style="color: #fcfdfd"></i>
+            <a>+91 9870579944, +91 9667661243, +91 9667681691</a>
+          </div>
+        </div>
+  
+        <div class="navbar h-nav">
+          <img src="logo.jpg" alt="Logo" class="logo" />
+  
+          <ul class="nav-list">
+            <li><a href="index.html" class="nav-link">Home</a></li>
+            <li><a href="aboutUs.html" class="nav-link">About Us</a></li>
+            <li>
+              <a href="ourproducts.html" class="nav-link">Our Products</a>
+            </li>
+            <li>
+              <a href="patentproduct.html" class="nav-link">Patent Products</a>
+            </li>
+            <li>
+              <a href="PCDpharma.html" class="nav-link">PCD Pharma Franchise</a>
+            </li>
+            <li>
+              <a href="thirdparty.html" class="nav-link">Third Party Manufacuring</a>
+            </li>
+            <li><a href="ContactUs.html" class="nav-link">Contact Us</a></li>
+            <!-- <li><i class="fa-solid fa-magnifying-glass fa-2xl"></i></li> -->
+          </ul>
+          <div class="burger">
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+          </div>
+  
+          <div class="dropdown-menu">
+            <ul >
+              <li><a href="index.html" class="nav-link">Home</a></li>
+              <li><a href="aboutUs.html" class="nav-link">About Us</a></li>
+              <li>
+                <a href="ourproducts.html" class="nav-link">Our Products</a>
+              </li>
+              <li>
+                <a href="PCDpharma.html" class="nav-link">PCD Pharma Franchise</a>
+              </li>
+              <li>
+                <a href="thirdparty.html" class="nav-link">Third Party Manufacuring</a>
+              </li>
+              <li><a href="ContactUs.html" class="nav-link">Contact Us</a></li>
+            </ul>
+          </div>
+        </div>
+      </header>
+
+      <section class="slideshow-container">
+        <div class="mySlides fade">
+          <img src="img1.jpg" style="width: 100%" />
+         
+        </div>
+
+        <div class="mySlides fade">
+          <img src="img2.jpg" style="width: 100%" />
+
+        </div>
+
+        <div style="text-align: center">
+          <span class="dot"></span>
+          <span class="dot"></span>
+        </div>
+      </section>
+      <div class="secondSection" style= "display: grid;  grid-template-columns: 1fr 1fr 1fr 1fr; place-items: center;">
+        <div class="card" id="card 1">
+          <h2>AYURVEDIC MEDICINE</h2>
+          <p>
+            Our Ayurvedic Medicine heals body with essence to nature. At Vinayak
+            Ayurveda, quality medicine fights diseases. Medicine comes in the
+            form of medicated oils, herbs, plant extracts, decoctions etc..
+          </p>
+          <a href="ourproducts.html" class="read-more">Read More</a>
+        </div>
+
+        <div class="card" id="card 2">
+          <h2>AYURVEDIC SYRUP</h2>
+          <p>
+            Range of Ayurvedic Syrups available at Vinayak Ayurveda brings
+            qualitative changes in the health condition. These syrups are made
+            using best knowledge of ancient Ayurveda.
+          </p>
+          <a href="ourproducts.html" class="read-more">Read More</a>
+        </div>
+
+        <div class="card" id="card 3">
+          <h2>AYURVEDIC CAPSULES</h2>
+          <p>
+            Capsule Range are manufactured after good research using herbs and
+            natural ingredients which help with fast recovery and they are
+            efficient as well.
+          </p>
+          <a href="ourproducts.html" class="read-more">Read More</a>
+        </div>
+
+        <div class="card" id="card 4">
+          <h2>AYURVEDIC POWDER</h2>
+          <p>
+            Get wide range of quality Ayurvedic Powder at Vinayak Ayurveda which
+            is manufactured under the observation of experts who have years of
+            experience in Ayurveda.
+          </p>
+          <a href="ourproducts.html " class="read-more">Read More</a>
+        </div>
+
+        <div class="card" id="card 5">
+          <h2>HERBAL COSMETICS</h2>
+          <p>
+            Enough of putting those harmful chemicals in your face & hair, it’s
+            time to try which nature offer you. Get exclusive Herbal Cosmetics
+            for healthy face & hair.
+          </p>
+          <a href="ourproducts.html" class="read-more">Read More</a>
+        </div>
+
+        <div class="card" id="card 6">
+          <h2>HERBAL JUICE</h2>
+          <p>
+            Our exclusive range of herbal Handmade soap is made with care for
+            better results. We have used the best quality material. Our
+            ingredients are natural and with no side effects.
+          </p>
+          <a href="ourproducts.html" class="read-more">Read More</a>
+        </div>
+
+        <div class="card" id="card 7">
+          <h2>HAND MADE SOAP</h2>
+          <p>
+            The Herbal Ayurvedic Juice which we offer are side effects free and
+            heal the body holistically & not just the symptoms.
+          </p>
+          <a href="ourproducts.html" class="read-more">Read More</a>
+        </div>
+
+        <div class="card" id="card 8">
+          <h2>HERBAL OINTMENT</h2>
+          <p>
+            Vinayak Ayurveda Provide you the best range of Herbal Ointment for
+            healthy, made by using the precious herbs and ingredients present in
+            nature.
+          </p>
+          <a href="ourproducts.html" class="read-more">Read More</a>
+        </div>
+      </div>
+
+      <br />
+
+      <section class="about">
+        <div class="about-container">
+          <h2>About Vinayak Ayurveda</h2>
+          <p>
+            Founded in the year 2023, We Vinayak Ayurveda Pvt. Ltd. are a
+            quality conscious organization with a traditional value of doing
+            business. We are into holistic science and firmly believe that
+            traditional medicinal treatments are the great asset for the
+            medicinal world. Therefore, our product range is based on
+            traditional values, ancient ayurvedic principles and holistic
+            living. Within One years of operation in the industry, we have
+            earned a distinctive position in the market because of our ayurvedic
+            products. Today, we are known as a reputed manufacturer, exporter
+            and supplier of some of the top known ayurvedic products in the
+            market including Herbal Tablets, Herbal Syrups, Pain Relief
+            Medicines, Herbal Capsules, Anti Diabetic Medicines, Ayurvedic Hair
+            Care Products, Herbal Powder Churna and many more.
+          </p>
+        </div>
+      </section>
+
+      <section id="index-PCD">
+        <img src="img5.jpg" alt="Image" />
+      </section>
+
+      <section id="index-PCD2">
+        <div id="PCD-part1">
+          <h4>AYURVEDIC THIRD PARTY MANUFACTURERS IN INDIA</h4>
+          <p>
+            We Vinayak Ayurveda Pvt. Ltd. provide a range of health solutions to
+            our clients and patients to enhance a healthy society. Our
+            manufacturing practices are flexible, enabling us to meet a wide
+            range of customer requirements. We embrace all the Pharma companies
+            who are venturing for Third Party Ayurvedic Medicines Manufacturing
+            in India. Since its inception our main goal has been to provide
+            benefits to our partners and the company itself. Our R&D team is the
+            foundation of our company, and we manufacture all of our products
+            according to WHO and GMP guidelines. In addition, our company has a
+            cutting-edge, state-of-the-art manufacturing facility as well as
+            laboratories for pharma product trials. So, Vinayak Ayurveda Pvt.
+            Ltd., Ayurvedic Third Party Manufacturers in India is offering you
+            the best Ayurvedic 3rd Party Manufacturing services in which include
+            a wide range of syrup, Gel Capsules, Drops, Powder, Hand Wash,
+            Shampoo, Skincare, Hair Care, Juice etc. which are 100% Safe &
+            Natural in nature.
+          </p>
+        </div>
+        <div id="PCD-part2">
+          <img src="img6.jpg" alt="Image" />
+        </div>
+      </section>
+
+      <section class="subscription-section">
+        <h2>Stay In Touch</h2>
+    
+          <form id="index-page-form">
+
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Enter your email"
+            required
+          />
+          <button type="submit" name="button">Submit</button>
+        </form>
+      </section>
+
+      <footer>
+        <div class="foot-panel2">
+          <ul>
+            <p>Quick Links</p>
+            <a href="index.html">Home</a>
+            <a href="aboutUs.html">About us</a>
+            <a href="ourproducts.html">Our Products</a>
+            <a href="PCDpharma.html">  PCD Pharma Franchise </a>
+            <a href="thirdparty.html">Third Party Manufacturing</a>
+            <a href="ContactUs.html">Contact Us</a>
+            
+          </ul>
+  
+          <ul>
+            <p>Services</p>
+            <a>Private Lables</a>
+            <a>Cosmetics thirdparty Manufacturing</a>
+            <a>Ayurvedic Medicine</a>
+            <ul class="footer-inner-list">
+              <li><a href=""><i class="fa-brands fa-instagram "></i></a></li>
+              <li><a href=""><i class="fa-brands fa-facebook "></i></a></li>
+              <li><a href=""><i class="fa-brands fa-x-twitter "></i></a></li>
+              <li><a href="https://wa.me/9870579944"><i class="fa-brands fa-whatsapp"></i></a></li>
+            </ul>
+          </ul>
+  
+          <ul>
+            <p>Contact Info</p>
+            <a> Vinayak Ayurveda Pvt. Ltd.</a>
+            <a>Plot No. 12 New DLF Industrial Area</a>
+            <a> Phase 1, Faridabad(Haryana)</a>
+            <a>Pin-121003 </a>
+            <a>Mobile: +91 9870579944</a>
+            <a>Email-info@vinayakayurveda.in</a>
+            
+          </ul>
+        </div>
+        <div class="foot-panel4">
+          <div class="pages">
+            <a>Conditions of Use</a>
+            <a>Privacy</a>
+            <a>Your Ads Privacy Choices</a>
+          </div>
+          <div class="copyright">
+            All Right Reserved @ vinayakayurveda.Web Design & Development By
+            Vinayak Ayurveda
+          </div>
+        </div>
+      </footer>
+      <?php 
+      include 'footer.php';
+      
+      ?>
+
+    </div>
+
+    <script>
+      let slideIndex = 0;
+      showSlides();
+
+      function showSlides() {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        let dots = document.getElementsByClassName("dot");
+        for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {
+          slideIndex = 1;
+        }
+        for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+        setTimeout(showSlides, 10000);
+      }
+    </script>
+
+    <script
+      type="module"
+      src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
+    ></script>
+    <script
+      nomodule
+      src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
+    ></script>
+    <script src="index.js"></script>
+    <script>
+
+      let url = 'https://script.google.com/macros/s/AKfycbxnRj92w-siMqg8Kgxq0DeBGR3rYeQ3jK0KEUvOh7k3GlS29FUBwrURQkR55praO02dfg/exec';
+      let form = document.querySelector("#index-page-form");
+
+      form.addEventListener("submit", (e) => {
+      
+        e.target.button.innerHTML = "Working"
+        let d = new FormData(form);
+            fetch(url, {
+                method: "POST",
+                body: d
+            }).then((res) => res.text()).then((finalRes) => {
+
+                    form.reset();
+                    e.target.button.innerHTML = "Saved";
+
+                    setTimeout(() => {
+                        e.target.button.innerHTML = "Submit";
+
+                    }, 2000)
+
+                })
+        e.preventDefault();
+
+      })
+
+
+    </script>
+  </body>
+</html>
